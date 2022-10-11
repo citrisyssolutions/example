1dac7a3f069372e8a2edaae76ebb41a97fc33d03
muthu <muthupandianthangarajan@gmail.com>
2022-10-11 17:14:39 +0530

    from config

--- airtabletest.py
+++ airtabletest.py
@@ -1,62 +1,20 @@
-from sqlalchemy.engine import create_engine
-from sqlalchemy.orm import relationship, backref, sessionmaker, registry, declarative_base
-
-from sqlalchemy import Text, Column, Integer, String, ForeignKey, Table, create_engine, MetaData
-metadata = MetaData()
-mapper_registry = registry()
+key = 'airtable://:keyvFVTECSngawPxK@appeGrlit5GKyPLX0'
+from airtable_orm import AirtableORM
 from dataclasses import dataclass
 
+orm = AirtableORM(key)
+session = orm.get_session()
+
 @dataclass
-class User:
-    Name: str
-    Price: str
-    Description: str
+class Room:
+    id: int
+    room_name: str
+    room_type: str
 
-users = Table(
-    "users",
-    metadata,
-    Column("Name", String(255), primary_key=True),
-    Column("Price", String(255)),
-    Column("Description", String(255))
-)
-
-# Base = declarative_base()
-
-# class MyTable(Base):
-#     __tablename__ = 'users'
-#     id = Column(Integer, primary_key=True)
-#     Name = Column(String(500))
-#     Price = Column(String(500))
-#     Description = Column(String(500))
-
-#     def __init__(self, Name, Price, Description):
-#         id = Column(Integer, primary_key=True)
-#         self.Name = Name
-#         self.Price = Price
-#         self.Description = Description
-
-
-engine = create_engine(
-    'airtable://:keyvFVTECSngawPxK@appeGrlit5GKyPLX0'
-)
-
-# print(engine.table_names())
-session = sessionmaker(bind=engine)()
-mapper_registry.map_imperatively(User, users)
-# res = session.execute("select * from users")
-# for re in res:
-#     print(re)
-
-new = User(
-    "muthu",
-    "pandian",
-    "description"
-)
-
-product = session.query(User).all()
-for p in product:
-    print(p.Name)
-
-session.add(new)
+new_entity = Room(2, "My name", "Dev")
+session.add(new_entity)
 session.commit()
-    
+
+d = session.query(Room).all()
+for _ in d:
+    print(_.room_name)
--- application/config.py
+++ application/config.py
@@ -1,16 +1,17 @@
 import os
 from application.providers.db.airtable_db_source import AirtableDBSource
 from application.providers.db.sqlite_db_source import SQLiteDBSource
-from application.providers.orm.airtable_orm import AirtableORM
+# from application.providers.orm.airtable_orm import AirtableORM
+from airtable_orm import AirtableORM
 from application.providers.orm.sqlalchemy_orm import SQLAlchemyORM
 
 
 class Config:
-    DB_CONNECTION_STRING = os.environ.get("DB_CONNECTION_STRING", "sqlite:////tmp/mydb_3.db")
-    DB_SOURCE = SQLiteDBSource(connection_string=DB_CONNECTION_STRING)
-    DB = SQLAlchemyORM(DB_SOURCE)
-    # DB_CONNECTION_STRING = os.environ.get("DB_CONNECTION_STRING", "airtable://:keyvFVTECSngawPxK@appeGrlit5GKyPLX0")
-    # DB_SOURCE = AirtableDBSource(connection_string=DB_CONNECTION_STRING)
-    # DB = AirtableORM(DB_SOURCE)
+    # DB_CONNECTION_STRING = os.environ.get("DB_CONNECTION_STRING", "sqlite:////tmp/mydb_3.db")
+    # DB_SOURCE = SQLiteDBSource(connection_string=DB_CONNECTION_STRING)
+    # DB = SQLAlchemyORM(DB_SOURCE)
+    DB_CONNECTION_STRING = os.environ.get("DB_CONNECTION_STRING", "airtable://:keyvFVTECSngawPxK@appeGrlit5GKyPLX0")
+    DB_SOURCE = AirtableDBSource(connection_string=DB_CONNECTION_STRING)
+    DB = AirtableORM(DB_SOURCE.connection_string)
     
     
